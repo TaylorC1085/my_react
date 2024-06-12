8afa0a1b6c0de830af112bffe49f8c0bc0ffc2cc
Taylor Courchaine <taylorcourchaine@Taylors-MacBook-Air.local>
2024-06-12 09:06:09 -0500

    Header and Footer completed

---
+++ my-react/src/layouts/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import LinkedIn from '../images/linkedin.png'
+import Github from '../images/github.png'
+import Email from '../images/email.webp'
+import '../styles/footer.css'
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <>
+            <footer>
+                <nav className='footer-nav'>
+                    <p className='footer-text'>© 2024 Taylor Courchaine</p>
+                    <a href='' className='footer-nav-item footer-nav-center-align'>about me</a>
+                    <a href='' className='footer-nav-item footer-nav-center-align'>experience</a>
+                    <a href='' className='footer-nav-item footer-nav-center-align'>contact</a>
+                </nav>
+            </footer>
+        </>
+    )
+  }
+}
+
--- my-react/src/pages/Home.js
+++ my-react/src/pages/Home.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 import React, { Component } from 'react'
 import Header from '../layouts/Header'
+=======
+import React, { Component } from 'react';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+>>>>>>> 933bfe9 (Header and Footer completed)
 
 export default class Home extends Component {
   render() {
@@ -7,7 +13,13 @@ export default class Home extends Component {
     return (
         <>
         <Header />
+<<<<<<< HEAD
         
+=======
+        <h1>Taylor Courchaine</h1>
+        <button>About Me</button>
+        <Footer />
+>>>>>>> 933bfe9 (Header and Footer completed)
         </>
     )
   }
