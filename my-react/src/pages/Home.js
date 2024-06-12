65d81f3f1b981d2d0d68b286e97e81c2a125e8e3
Taylor Courchaine <taylorcourchaine@Taylors-MacBook-Air.local>
2024-06-12 10:59:36 -0500

    Navigation completed

--- my-react/src/pages/Home.js
+++ my-react/src/pages/Home.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import Header from '../layouts/Header'
-=======
 import React, { Component } from 'react';
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
->>>>>>> 933bfe9 (Header and Footer completed)
 
 export default class Home extends Component {
   render() {
@@ -13,13 +10,10 @@ export default class Home extends Component {
     return (
         <>
         <Header />
-<<<<<<< HEAD
         
-=======
         <h1>Taylor Courchaine</h1>
         <button>About Me</button>
         <Footer />
->>>>>>> 933bfe9 (Header and Footer completed)
         </>
     )
   }
